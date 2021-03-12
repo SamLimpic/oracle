7cf58fe4153b2adb2b6b468fe55042e24caf5cac
Sam Limpic <samlimpic@gmail.com>
2021-03-12 01:26:50 -0700

    Full functionality!

--- app.js
+++ app.js
@@ -183,35 +183,46 @@ let spread = []
 
 let idArray = []
 
+let input = document.getElementById("inputId");
+
+input.oninput = function (e) {
+  e.target.value = e.target.value.replace(' ', '_');
+}
+
 function saveSpread(event) {
   event.preventDefault()
   let form = event.target
   let name = form.name.value
   window.localStorage.setItem(name, JSON.stringify(spread))
   form.reset()
-  reloadPage
+  reloadPage()
 }
 
 function loadSpreads() {
   let saveData = []
-  for (let i = 0; i < localStorage.length; i++) {
-    saveData.push(window.localStorage.key(i))
-  }
   let saveDataListElement = document.getElementById("saved-readings")
   let saveDataTemplate = ""
-  saveData.forEach(draw => {
-    let drawIndex = saveData.indexOf(draw)
+  for (let i = 0; i < localStorage.length; i++) {
+    saveData.push(window.localStorage.key(i))
+    let noSpaces = saveData[i].replace('_', ' ')
     saveDataTemplate += `
-      <a onClick= loadSave("${saveData[drawIndex]}")> ${saveData[drawIndex]} </a>
+      <a onClick= loadSave("${saveData[i]}")> ${noSpaces} </a>
     `
-  })
+  }
   saveDataListElement.innerHTML = saveDataTemplate
 }
 
 function loadSave(string) {
   spread = JSON.parse(window.localStorage.getItem(string))
+  nameLength()
   drawSpread()
-  console.log(string)
+  document.getElementById("saved-reading").innerText = "Your Saved Reading"
+  document.getElementById("save-input").classList.add("hidden")
+}
+
+function redraw(_number) {
+  loadSpreads()
+  generateSpread(_number)
 }
 
 function reloadPage() {
@@ -240,63 +251,77 @@ function murkState() {
   console.log(Math.floor(Math.random() * 10))
 }
 
-function runePosition(_number) {
-  let spreadName = ""
+function nameLength() {
+  let spreadName = rune.reading
+  length = spread.length
+  if (length == 1) {
+    spreadName = "Rune of the Day"
+  } else if (length == 2) {
+    spreadName = "What Will Be"
+  } else if (length == 3) {
+    spreadName = "Past Present Future"
+  } else if (length == 4) {
+    spreadName = "Four Points"
+  } else if (length == 5) {
+    spreadName = "Runic Cross"
+  } else if (length == 7) {
+    spreadName = "Long Journey"
+  } else if (length == 9) {
+    spreadName = "Nine Realms"
+  }
+  document.getElementById("spread-name").innerText = spreadName
+}
+
+
+function runePosition() {
+  length = spread.length
   let spreadId = ""
-  if (_number == 1) {
-    spreadName = "Single Rune Reading"
+  if (length == 1) {
     spreadId = "one"
     spread[0].position = "Daily Draw"
-  } else if (_number == 2) {
-    spreadName = "Two Rune Reading"
+  } else if (length == 2) {
     spreadId = "two"
     spread[0].position = "Circumstances"
     spread[1].position = "Outcome"
-  } else if (_number == 3) {
-    spreadName = "Three Rune Reading"
+  } else if (length == 3) {
     spreadId = "three"
     spread[0].position = "Past"
     spread[1].position = "Present"
     spread[2].position = "Future"
-  } else if (_number == 4) {
-    spreadName = "Four Rune Reading"
+  } else if (length == 4) {
     spreadId = "four"
-    spread[0].position = "Placeholder"
-    spread[1].position = "Placeholder"
-    spread[2].position = "Placeholder"
-    spread[3].position = "Placeholder"
-  } else if (_number == 5) {
-    spreadName = "Five Rune Reading"
+    spread[0].position = "North"
+    spread[1].position = "East"
+    spread[2].position = "West"
+    spread[3].position = "South"
+  } else if (length == 5) {
     spreadId = "five"
-    spread[0].position = "Placeholder"
-    spread[1].position = "Placeholder"
-    spread[2].position = "Placeholder"
-    spread[3].position = "Placeholder"
-    spread[4].position = "Placeholder"
-  } else if (_number == 7) {
-    spreadName = "Seven Rune Reading"
+    spread[0].position = "Past Influence"
+    spread[1].position = "Obstacles"
+    spread[2].position = "Outcome"
+    spread[3].position = "Benefits"
+    spread[4].position = "Future Possibilities"
+  } else if (length == 7) {
     spreadId = "seven"
-    spread[0].position = "Placeholder"
-    spread[1].position = "Placeholder"
-    spread[2].position = "Placeholder"
-    spread[3].position = "Placeholder"
-    spread[4].position = "Placeholder"
-    spread[5].position = "Placeholder"
-    spread[6].position = "Placeholder"
-  } else if (_number == 9) {
-    spreadName = "Nine Rune Reading"
+    spread[0].position = "Hidden Past"
+    spread[1].position = "Past Influence"
+    spread[2].position = "Present Obstacles"
+    spread[3].position = "Present Attitude"
+    spread[4].position = "Potential Action"
+    spread[5].position = "Future Obstacles"
+    spread[6].position = "Hidden Future"
+  } else if (length == 9) {
     spreadId = "nine"
-    spread[0].position = "Placeholder"
-    spread[1].position = "Placeholder"
-    spread[2].position = "Placeholder"
-    spread[3].position = "Placeholder"
-    spread[4].position = "Placeholder"
-    spread[5].position = "Placeholder"
-    spread[6].position = "Placeholder"
-    spread[7].position = "Placeholder"
-    spread[8].position = "Placeholder"
+    spread[0].position = "Niflheim"
+    spread[1].position = "Asgard"
+    spread[2].position = "Vanaheim"
+    spread[3].position = "Ljosalfheim"
+    spread[4].position = "Midgardr"
+    spread[5].position = "Helheim"
+    spread[6].position = "Muspellheim"
+    spread[7].position = "Svartalheim"
+    spread[8].position = "Jotunheim"
   }
-  document.getElementById("spread-name").innerText = spreadName
   document.getElementById("redraw").classList.remove("hidden")
   document.getElementById(spreadId).classList.remove("hidden")
 }
@@ -310,13 +335,12 @@ function getStarted() {
 
 function generateSpread(_number) {
   spread = []
-  let spreadName = ""
   randomIdArray(_number)
   for (let i = 0; i < _number; i++) {
     spread[i] = {
-      reading: spreadName,
       murk: "false",
       position: "",
+      place: i,
       id: futhark[idArray[i]].id,
       name: futhark[idArray[i]].name,
       upright: futhark[idArray[i]].upright,
@@ -326,54 +350,110 @@ function generateSpread(_number) {
       spread[i].murk = "true"
     }
   }
-  runePosition(_number)
+  runePosition()
+  nameLength()
   drawSpread()
 }
 
 function drawSpread() {
   getStarted()
-  let spreadListElement = document.getElementById("spread")
-  let spreadTemplate = ""
+  let spreadOneListElement = document.getElementById("spread-one")
+  let spreadOneTemplate = ""
+  let spreadTwoListElement = document.getElementById("spread-two")
+  let spreadTwoTemplate = ""
+  let spreadThreeListElement = document.getElementById("spread-three")
+  let spreadThreeTemplate = ""
   let length = spread.length
   for (let i = 0; i < length; i++) {
     let rune = spread[i]
     let runeCard = 'rune-card-upright'
     let labelColor = 'label-upright'
+    let descriptionColor = 'description-upright'
     let glow = 'upright'
     let hide = 'hidden'
     let show = ''
     if (rune.murk == "true" && rune.name != "Wyrd") {
       runeCard = 'rune-card-inverted'
       labelColor = 'label-inverted'
+      descriptionColor = 'description-inverted'
       glow = 'inverted'
-      hide = 'text-danger'
+      hide = ''
       show = 'hidden'
     }
     if (rune.name == "Wyrd") {
       runeCard = 'rune-card-weird'
       labelColor = 'label-weird'
+      descriptionColor = 'description-weird'
       glow = 'weird'
     }
-    spreadTemplate += `
-    <div id="spread" class="${runeCard} m-1 shadow">
-      <button class="title-text position-label center mb-1 ${labelColor}" disabled>
+    if ((length <= 3) || (length == 4 && rune.place == 0) || (length == 5 && rune.place <= 1) || (length == 7 && rune.place <= 1) || (length == 9 && rune.place <= 2)) {
+      spreadOneTemplate += `
+    <div class="rune-card ${runeCard} m-1 shadow">
+
+      <button class="position-card title-text position-label center mb-1 ${labelColor}" disabled>
         <large> ${rune.position}</large>
       </button>
-      <img class = "${glow} center m-1" src="Runes/${rune.name}.png">
-      <label class = "rune-text">
-        <p class = "text-center">${rune.name}</p>
-      </label>
-      <button class="label-text rune-label center ${labelColor}" disabled>
+
+      <button class="name-card title-text position-label center mb-1 ${labelColor}" disabled>
+        <large>${rune.name} </large>
+      </button>
+      
+      <img class = "${glow} center rune-img" src="Runes/${rune.name}.png">
+
+      <button class="label-text center ${descriptionColor} rune-description" disabled>
         <p class = ${show}> ${rune.upright} Throw in a lot of sample text that will probably emcompass several lines of code to test the card capacity </p>
         <p class = ${hide}> ${rune.murkstave} Throw in a lot of sample text that will probably emcompass several lines of code to test the card capacity </p>
       </button>
+  
     </div>
   `
+    } else if ((length == 4 && rune.place <= 2) || (length == 5 && rune.place == 2) || (length == 7 && rune.place <= 4) || (length == 9 && rune.place <= 5)) {
+      spreadTwoTemplate += `
+    <div class="rune-card ${runeCard} m-1 shadow">
 
+      <button class="position-card title-text position-label center mb-1 ${labelColor}" disabled>
+        <large> ${rune.position}</large>
+      </button>
+
+      <button class="name-card title-text position-label center mb-1 ${labelColor}" disabled>
+        <large>${rune.name} </large>
+      </button>
+      
+      <img class = "${glow} center rune-img" src="Runes/${rune.name}.png">
+
+      <button class="label-text center ${descriptionColor} rune-description" disabled>
+        <p class = ${show}> ${rune.upright} Throw in a lot of sample text that will probably emcompass several lines of code to test the card capacity </p>
+        <p class = ${hide}> ${rune.murkstave} Throw in a lot of sample text that will probably emcompass several lines of code to test the card capacity </p>
+      </button>
+  
+    </div>
+  `
+    } else if ((length == 4 && rune.place == 3) || (length == 5 && rune.place <= 4) || (length == 7 && rune.place <= 6) || (length == 9 && rune.place <= 8)) {
+      spreadThreeTemplate += `
+    <div class="rune-card ${runeCard} m-1 shadow">
+
+      <button class="position-card title-text position-label center mb-1 ${labelColor}" disabled>
+        <large> ${rune.position}</large>
+      </button>
+
+      <button class="name-card title-text position-label center mb-1 ${labelColor}" disabled>
+        <large>${rune.name} </large>
+      </button>
+      
+      <img class = "${glow} center rune-img" src="Runes/${rune.name}.png">
+
+      <button class="label-text center ${descriptionColor} rune-description" disabled>
+        <p class = ${show}> ${rune.upright} Throw in a lot of sample text that will probably emcompass several lines of code to test the card capacity </p>
+        <p class = ${hide}> ${rune.murkstave} Throw in a lot of sample text that will probably emcompass several lines of code to test the card capacity </p>
+      </button>
+  
+    </div>
+  `
+    }
   }
-  spreadListElement.innerHTML = spreadTemplate
+  spreadOneListElement.innerHTML = spreadOneTemplate
+  spreadTwoListElement.innerHTML = spreadTwoTemplate
+  spreadThreeListElement.innerHTML = spreadThreeTemplate
 }
-
-
 
 loadSpreads()
